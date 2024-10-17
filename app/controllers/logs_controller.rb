00f084721612706b8234e815bd4ddab80f76b526
Maimunah Adeniji-Bello <maimunahbello05@gmail.com>
2024-10-17 18:07:19 +0100

    user can log weights and reps

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,21 @@
 class LogsController < ApplicationController
+  def new
+    @split = Split.find(params[:split_id])
+    @programme = @split.programme
+    @splits = @programme.splits
+    @log = Log.new
+  end
+
+  def create
+    @workout = Workout.find(params[:workout_id])
+    @split_exercise = SplitExercise.find(params[:split_exercise_id])
+    @log = Log.new(log_params)
+    @log.save
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:weight, :repetitions)
+  end
 end
