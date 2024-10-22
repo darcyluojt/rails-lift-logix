dc35c6c5a497730368eddbeedbac2fc5c094094d
Darcy Luo <darcyluojt@gmail.com>
2024-10-22 16:40:53 +0100

    calendar view show users' workout dates

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,4 +3,10 @@ class Workout < ApplicationRecord
   # belongs_to :programme, optional: true
   has_many :logs, dependent: :destroy
   has_many :split_exercises, through: :logs
+
+  def start_time
+    # set a default start time of each workout for the calendar view
+    # this is used in the calendar view to display the workout start time
+    date
+  end
 end
